8f109c76baa9c0502a743d13e6bd12e8ce64dba0
Parker Wightman <parkerwightman@gmail.com>
2011-11-30 16:43:46 -0700

    Adding more testing code to make sure NFA's are equal, added Makefile back to .gitignore

--- test_nfa.cpp
+++ test_nfa.cpp
@@ -19,6 +19,27 @@ typedef enum {
 /* Declaration for the assert method defined below */
 void assert_nfa(Nfa* nfa, QString str, bool expected, TestType type);
 
+static const char* debugString(QSet<QString>* states)
+{
+  QList<QString> list = states->toList();
+  QString str = "{ ";
+  int len = list.size();
+  for(int i = 0; i < len; i++)
+  {
+    str += list[i];
+    if(i != len - 1)
+      str += ", ";
+  }
+
+  str += " }";
+
+  return str.toStdString().c_str();
+}
+/* A nice, pretty string about which type of test was run */
+const char* testTypeStr(TestType type)
+{
+  return (type == TestTypeSequential ? "Sequential" : "Parallel");
+}
 /*
  * Defines the NFA: even 1's and 0's (?)
  */
@@ -84,23 +105,52 @@ void test_nfa_1(TestType type)
  */
 void assert_nfa(Nfa* nfa, QString str, bool expected, TestType type)
 {
-  QVector<int> results;
-  if((type == TestTypeSequential) || (type == TestTypeBoth))
-     results.push_back(nfa->isValidString(str, false) == expected);
-  if((type == TestTypeParallel) || (type == TestTypeBoth))
-     results.push_back(nfa->isValidString(str, true) == expected);
+  /* A result is made up of the result (bool) and what type was run (TestType) */
+  QVector<QPair<bool,TestType> > results;
 
+  /* Run the tests for the correct type */
+  if((type == TestTypeSequential) || (type == TestTypeBoth))
+    results.push_back(
+      QPair<bool,TestType>(
+        (nfa->isValidString(str, false) == expected), TestTypeSequential
+      )
+    );
+   
+  if((type == TestTypeParallel) || (type == TestTypeBoth))
+    results.push_back(
+      QPair<bool,TestType>(
+        (nfa->isValidString(str, true) == expected), TestTypeParallel
+      )
+    );
+
+  /* Assert the true/false results of all the tests that were run */
   for(int i = 0; i < results.size(); i++)
   {
-    if(results[i])
-      printf("PASSED: %s\n", str.toStdString().c_str());
+    if(results[i].first)
+      printf("PASSED: %s, %s\n", str.toStdString().c_str(), testTypeStr(results[i].second));
     else
     {
       printf("FAILED: %s\n", str.toStdString().c_str());
       printf("\tExpected: %s\n", (expected ? "true" : "false"));
       printf("\tReceived: %s\n", (expected ? "false" : "true"));
+      printf("\tType: %s\n", testTypeStr(results[i].second));
     }
   }
+
+  /* When testing both, this is a more robust test of making sure the returned sets were equal */
+  if (type == TestTypeBoth) 
+  {
+    QSet<QString>* first = nfa->runNfa(str);
+    QSet<QString>* second = nfa->runNfaP(str);
+    if((*first) != (*second))
+    {
+      printf("FAILED: %s\n", str.toStdString().c_str());
+      printf("\tParallel and Sequential NFA's do not match:");
+      printf("\tSequential : %s\n", debugString(first));
+      printf("\tParallel   : %s\n", debugString(second));
+    }
+  }
+  
 }
 
 /*
